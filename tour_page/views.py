241045a3a8e9e6c07802f4d95534b2ea058647c7
Artem Moskowsky <artemij.mosckowsky.01062008@gmail.com>
2024-11-16 12:01:39 +0200

    Bug fix

--- tour_page/views.py
+++ tour_page/views.py
@@ -1,9 +1,10 @@
 import flask
 from .models import Trip
 from main.settings import DATABASE
+import flask_login
 
 def render_tour():
-    print(Trip.query.all())
+    
 
     # trip_to_turkey = Trip(title = 'Trip to Turkey', date = '13.10.2025', country = 'Turkey', price = '100$', description = 'abcd')
 
@@ -21,8 +22,16 @@ def render_tour():
     # DATABASE.session.add(trip_to_italy)
     # DATABASE.session.commit()
 
-    
-    return flask.render_template(template_name_or_list = 'tour.html', all_trips = Trip.query.all())
+    try:
+        flask_login.current_user.username
+        return flask.render_template(template_name_or_list = 'tour.html', all_trips = Trip.query.all())
+    except:
+        return flask.redirect("/")
 
 def show_trip(id):
-    return flask.render_template(template_name_or_list = "tour_show.html", countries = Trip.query.get(id))
+    try:
+        flask_login.current_user.username
+        return flask.render_template(template_name_or_list = "tour_show.html", countries = Trip.query.get(id))
+    except:
+        return flask.redirect("/")
+    
